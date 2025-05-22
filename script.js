6bdf3bf5fa5902a535da67263961da91a746047c
Koajanaku <koajanaku@cps.edu>
2025-05-22 06:11:33 +0000

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+let input = document.querySelector(".input");
+let clearButton = document.querySelector(".clear");
+let equalsButton = document.querySelector(".equals");
+let result = document.querySelector(".result");
+
+
+const calculateDoggieAge = (humanAge) => {
+let doggieAge = humanAge * 7;
+result.innerHTML = "Your dog is " + doggieAge + " years old in human years.";
+console.log ("Your dog is " + doggieAge + " years old in human years.");   
+}
+
+
+equalsButton.addEventListener("click", () => { 
+ let userInput = input.value;
+ console.log (userInput);
+calculateDoggieAge(Number(userInput));
+});
+
+clearButton.onclick = function() {
+  input.value = "";
+  result.innerHTML = "";
+}
